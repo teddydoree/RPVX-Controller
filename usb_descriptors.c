d68e0c4de212246d2fe26c8e98cd7653dbf8ba6c
Theodore Molina <theodoremari@gmail.com>
2025-03-03 07:09:25 -0500

    changed device ID to RPVX Controller

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -190,10 +190,10 @@
  // array of pointer to string descriptors
  char const *string_desc_arr[] =
  {
-   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-   "TinyUSB",                     // 1: Manufacturer
-   "TinyUSB Device",              // 2: Product
-   NULL,                          // 3: Serials will use unique ID if possible
+   (const char[]) { 0x09, 0x04 },   // 0: is supported language is English (0x0409)
+   "Theodore Molina",               // 1: Manufacturer
+   "RPVX Controller",               // 2: Product
+   NULL,                            // 3: Serials will use unique ID if possible
  };
  
  static uint16_t _desc_str[32 + 1];
