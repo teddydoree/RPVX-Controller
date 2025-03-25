7646174593ad32a0799d32a8e38ce888c1f12439
Theodore Molina <theodoremari@gmail.com>
2025-03-24 23:33:16 -0400

    changed encoder debounce delay to 5000us, much more stable

--- RPVX.c
+++ RPVX.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "RPVX.h"
+#include "hardware/pwm.h"
 
 #include "bsp/board_api.h"
 #include "tusb.h"
-
 #include "usb_descriptors.h"
 
 #define START 22
@@ -19,6 +19,11 @@
 #define VOL_R_A 0
 #define VOL_R_B 1
 
+//#define RED_RGB 18
+//#define GREEN_RGB 19
+//#define BLUE_RGB 20
+//#define CYCLE_TIME_MS 5000
+
 #define START_KEY HID_KEY_1
 #define BT_A_KEY HID_KEY_D
 #define BT_B_KEY HID_KEY_F
@@ -27,18 +32,25 @@
 #define FX_L_KEY HID_KEY_C
 #define FX_R_KEY HID_KEY_M
 
+#define DEBOUNCE_DELAY_ENC_US 5000
+//#define DEBOUNCE_DELAY_BTN_US 5000
+#define MOUSE_DELTA 10
+
+//bool buttons_sent = true;
+
 // [VOL-L, VOL-R] [pin A, pin B]
 bool vol[2][2];
 
 // [VOL-L, VOL-R] [previous direction, current direction]
 bool vol_directions[2][2] = { {0,0}, {0,0} };
 
-int8_t delta[2] = {0, 0};
-
-int64_t prev_time[2] = {0, 0};
+// [VOL-L, VOL-R]
+int8_t delta[2] = {0, 0}; // mouse delta
+int64_t prev_time_enc[2] = {0, 0}; // time of previous encoder change
+//int64_t prev_time_btn[7] = {0, 0, 0, 0, 0, 0, 0};
 
 // [START, BT-A, BT-B, BT-C, BT-D, FX_L, FX_R]
-bool button_states[7];
+bool button_states[7] = {0, 0, 0, 0, 0, 0, 0};
 uint8_t button_maps[7] = {
     START_KEY,
     BT_A_KEY,
@@ -49,10 +61,21 @@ uint8_t button_maps[7] = {
     FX_R_KEY
 };
 
+
 void init_pico ()
 {
+    init_button(START);
+    init_button(BT_A);
+    init_button(BT_B);
+    init_button(BT_C);
+    init_button(BT_D);
+    init_button(FX_L);
     init_button(FX_R);
+    init_encoder(VOL_L_A, VOL_L_B);
     init_encoder(VOL_R_A, VOL_R_B);
+    //init_pwm(RED_RGB);
+    //init_pwm(GREEN_RGB);
+    //init_pwm(BLUE_RGB);
 }
 
 int main()
@@ -80,49 +103,65 @@ int main()
             gpio_get(FX_L),
             gpio_get(FX_R)
         };
+        // temporarily store current VOL values (encoder states)
         bool vol_new[2][2] = {
             {gpio_get(VOL_L_A), gpio_get(VOL_L_B)},
             {gpio_get(VOL_R_A), gpio_get(VOL_R_B)}
         };
 
-        // prints direction if encoder states are valid and new
+        // stores direction if encoder states are valid and different
         for (int i = 0; i < 2; ++i) {
+
             // invalid if 00 -> 11 or 11 -> 00
             if ((vol[i][0]!=vol_new[i][0]) && (vol[i][1]!=vol_new[i][1])) {
                 //printf("Invalid state change.\n\n");
             }
-            // no change
-            else if ((vol[i][0]==vol_new[i][0]) && (vol[i][1]==vol_new[i][1])) {}
-            // valid change, grab direction
-            else {
-                vol_directions[i][1] = encoder_direction(vol_new[i][0],vol_new[i][1], vol[i][0],vol[i][1]);
-                //if (vol_directions[i][0] == vol_directions[i][1]){
-                    if(vol_directions[i][1]) {
-                        
-                        //printf("R / CW\t\t-->\n\n");
-                        //delta[i] = 10;d
-                        //busy_wait_us(200);
 
-                        if (time_us_64() > (prev_time[i] + 200)) {
-                            delta[i] = 10;
-                            prev_time[i] = time_us_64();
+            // no change, do nothing
+            else if ((vol[i][0]==vol_new[i][0]) && (vol[i][1]==vol_new[i][1])) {}
+
+            // valid change
+            else {
+
+                // grabs direction if outside of debounce window
+                if (time_us_64() > (prev_time_enc[i] + DEBOUNCE_DELAY_ENC_US)) {
+                    vol_directions[i][1] = encoder_direction(vol_new[i][0],vol_new[i][1], vol[i][0],vol[i][1]);
+
+                    //if (vol_directions[i][0] == vol_directions[i][1]) { //----buffer for inputs----------- 
+                        if(vol_directions[i][1]) {
+                            
+                            //printf("R / CW\t\t-->\n\n");
+                            delta[i] = MOUSE_DELTA;
+
+                            /*
+                            if (time_us_64() > (prev_time_enc[i] + debounce_delay_us)) {
+                                delta[i] = MOUSE_DELTA;
+                                prev_time_enc[i] = time_us_64();
+                            }
+                            else {}
+                            */
                         }
-                        else {}
-                    }
-                    else {
-                        //printf("L / CCW\t<--\n\n");
-                        //delta[i] = -10;
-                        //busy_wait_us(200);
-                        if (time_us_64() > (prev_time[i] + 200)) {
-                            delta[i] = -10;
-                            prev_time[i] = time_us_64();
+                        else {
+                            //printf("L / CCW\t<--\n\n");
+                            delta[i] = -MOUSE_DELTA;
+
+                            /*
+                            if (time_us_64() > (prev_time_enc[i] + DEBOUNCE_DELAY_ENC_US)) {
+                                delta[i] = -MOUSE_DELTA;
+                                prev_time_enc[i] = time_us_64();
+                            }
+                            else{}
+                            */
                         }
-                        else{}
-                    }
-                //}
-                vol_directions[i][0] = vol_directions[i][1];
+                    //}
+                    vol_directions[i][0] = vol_directions[i][1];
+                    prev_time_enc[i] = time_us_64();
+                }
+                // ignores all signals in debounce window (in theory)
+                else {}
             }
         }
+        // send mouse report if any changes
         if (delta[0] != 0 || delta[1] != 0)
             send_mouse_report(); 
 
@@ -136,26 +175,42 @@ int main()
             }
         }
 
-        bool btn_changed = false;
+        //bool btn_changed = false;
+        
         // check button states for changes
         for (int i = 0; i < 7; ++i) {
-            //if (button_states[i] != button_new_states[i]) {
+            if ( (button_states[i] != button_new_states[i]) ) {
                 button_states[i] = button_new_states[i];
-                btn_changed = true;
-            //}
+                //btn_changed = true;
+            }
         }
         
+        /*
+        // check button states for changes with debounce
+        for (int i = 0; i < 7; ++i) {
+                if (time_us_64() > (prev_time_btn[i] + DEBOUNCE_DELAY_BTN_US)) {
+                    if ( (button_states[i] != button_new_states[i]) && buttons_sent) {
+                        button_states[i] = button_new_states[i];  
+                        prev_time_btn[i] = time_us_64();
+                        btn_changed = true;
+                    }
+                }
+            }
+            */
+
         // print button states if any changes
-        //if (btn_changed) {
+        //if (btn_changed || !buttons_sent) {
             //printf("Button states: %d %d %d %d %d %d %d\n", button_states[0], button_states[1], button_states[2],
             //        button_states[3], button_states[4], button_states[5], button_states[6]);
             //printf("VOL-L: %d %d\nVOL-R: %d %d\n\n",
             //        vol[0][0], vol[0][1], vol[1][0], vol[1][1]);
-            send_kb_report();
+            send_kb_report(); // ***** always send kb report, let windows handle debounce :D ************
         //}
         
     }
-}
+} //-----------------end of main--------------------------------
+
+
 void init_button (int pin)
 {
     gpio_set_function (pin, GPIO_FUNC_SIO);
@@ -173,6 +228,14 @@ void init_encoder (int a, int b)
     gpio_set_dir (b, GPIO_IN);
 }
 
+void init_pwm(int pin) {
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    int slice = pwm_gpio_to_slice_num(pin);
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_wrap(&config, 65535);  // 16-bit resolution
+    pwm_init(slice, &config, true);
+}
+
 // returns 0 for CCW, 1 for CW turns
 bool encoder_direction (bool a, bool b, bool oldA, bool oldB)
 {
@@ -187,8 +250,10 @@ bool encoder_direction (bool a, bool b, bool oldA, bool oldB)
 void send_kb_report()
 {
     // skips report if HID not ready
-    if ( !tud_hid_ready() ) return;
-
+    if ( !tud_hid_ready() ) {
+        //buttons_sent = false;
+        return;
+    }
     uint8_t keycode[6] = { 0 };
     int k = 0;
     for (int i = 0; i < 7; ++i)
@@ -201,6 +266,7 @@ void send_kb_report()
     }
 
     tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+    //buttons_sent = true;
 }
 
 // sends mouse HID report via TinyUSB
--- RPVX.h
+++ RPVX.h
@@ -2,6 +2,8 @@ void init_button (int pin);
 
 void init_encoder (int a, int b);
 
+void init_pwm(int pin);
+
 bool encoder_direction (bool a, bool b, bool newA, bool newB);
 
 void send_kb_report();
